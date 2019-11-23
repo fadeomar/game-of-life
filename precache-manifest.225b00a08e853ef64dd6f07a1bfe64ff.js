9ff9c8d3dd78d69e750f9991beaacca9545eef74
fadeomar <fadeomar2015@gmail.com>
2019-11-23 13:35:18 +0200

    Updates

---
+++ precache-manifest.225b00a08e853ef64dd6f07a1bfe64ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca0077aec075750768359d466d23cfd",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "50ab92d741d8fcbb0690",
+    "url": "/game-of-life/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "13c57c5f0d6048778e96",
+    "url": "/game-of-life/static/css/main.de6bd299.chunk.css"
+  },
+  {
+    "revision": "50ab92d741d8fcbb0690",
+    "url": "/game-of-life/static/js/2.3e0ff573.chunk.js"
+  },
+  {
+    "revision": "13c57c5f0d6048778e96",
+    "url": "/game-of-life/static/js/main.950354d9.chunk.js"
+  },
+  {
+    "revision": "6c337d97cb58ee876942",
+    "url": "/game-of-life/static/js/runtime-main.51be29ed.js"
+  }
+]);
